cb613a827474ddcea4769aa7e35c3dbf655a9440
israil-fakir <israilfakirrobin@gmail.com>
2025-02-24 22:39:18 +0600

    add topology sort

--- Class_4/DFS_task.py
+++ Class_4/DFS_task.py
@@ -18,12 +18,13 @@ class DFS:
         self.goal = None
         self.goal_level = 999999
         self.state = 0
+        self.topo_order = []  
 
-    def init(self):   #-------
+    def init(self):   
         while True:
             #N = int(input("Enter the N value:"))
-            N = ny.random.randint(4,8)
-            print("The Grid size : ",N)
+            N = ny.random.randint(4, 8)
+            print("The Grid size : ", N)
             # graph = [
             #     [0, 0, 1, 0, 1],
             #     [0, 1, 1, 1, 1],
@@ -31,15 +32,15 @@ class DFS:
             #     [1, 1, 0, 1, 1],
             #     [1, 0, 0, 0, 1]
             # ]
-            graph = ny.random.randint(0,2,(N,N))
-            print("The graph :",graph)
+            graph = ny.random.randint(0, 2, (N, N))
+            print("The graph :", graph)
             self.N = len(graph)
 
-            source_x = ny.random.randint(0,N)
-            print("random source x",source_x)
+            source_x = ny.random.randint(0, N)
+            print("random source x", source_x)
         
-            source_y = ny.random.randint(0,N)
-            print("random source y",source_y)
+            source_y = ny.random.randint(0, N)
+            print("random source y", source_y)
             # source_x = 0
             # source_y = 2
 
@@ -50,10 +51,10 @@ class DFS:
                 continue
            
             
-            goal_x = ny.random.randint(0,N)
-            print("random goal x",goal_x)
-            goal_y = ny.random.randint(0,N)
-            print("random goal y",goal_y)
+            goal_x = ny.random.randint(0, N)
+            print("random goal x", goal_x)
+            goal_y = ny.random.randint(0, N)
+            print("random goal y", goal_y)
 
             if graph[goal_x][goal_y] == 0:
                 print("goal is invalided")
@@ -68,14 +69,16 @@ class DFS:
             self.source = Node(source_x, source_y, 0)
             self.goal = Node(goal_x, goal_y, self.goal_level)
             self.st_dfs(graph, self.source)
-
+            
             if self.found:
                 print("Goal found")
                 print("Number of moves required =", self.goal.depth)
-                return 0
             else:
                 print("Goal cannot be reached from the starting block")
-
+            
+            print("Topological Order:", self.topo_order)
+            return
+    
     def print_direction(self, m, x, y):
         if m == 0:
             print("Moving Down ({}, {})".format(x, y))
@@ -85,7 +88,7 @@ class DFS:
             print("Moving Right ({}, {})".format(x, y))
         else:
             print("Moving Left ({}, {})".format(x, y))
-
+    
     def st_dfs(self, graph, u):
         graph[u.x][u.y] = 0
         for j in range(self.directions):
@@ -95,18 +98,20 @@ class DFS:
             if (0 <= v_x < self.N) and (0 <= v_y < self.N) and graph[v_x][v_y] == 1:
                 v_depth = u.depth + 1
                 self.print_direction(j, v_x, v_y)
-
+                
                 if v_x == self.goal.x and v_y == self.goal.y:
                     self.found = True
                     self.goal.depth = v_depth
                     return
-
+                
                 child = Node(v_x, v_y, v_depth)
                 self.st_dfs(graph, child)
-
+                
                 if self.found:
                     return
-
+        
+        self.topo_order.append((u.x, u.y))  # Add node to topological order
+    
 def main():
     d = DFS()
     d.init()
