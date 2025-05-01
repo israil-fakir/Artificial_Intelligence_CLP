5c92aec996db29e51c4cbbd2ac366ec7b5d5056e
israil-fakir <israilfakirrobin@gmail.com>
2025-05-01 21:24:46 +0600

    add new code

--- code_practice/Variables.py
+++ code_practice/Variables.py
@@ -36,3 +36,32 @@ print(p, q, r)
 print(p+q+r)
 
 
+num = 10
+st_nam = " Opi "
+# print(num + st_nam) #not working
+
+print(str(num) + st_nam)
+
+# Global variable
+
+val = 44
+
+def my_fun():
+    print(val)
+    
+my_fun()
+
+
+def fun():
+    my_weight = 64
+    
+# print(my_weight) the my_weight is not found in globaly
+
+def new_fun():
+
+    global score 
+    score = 46
+    print("Print from function", score)
+new_fun()
+print("print from outside the function", score)
+
