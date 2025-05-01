12e69d732176b99f7098f0db304a55be418a87a1
israil-fakir <israilfakirrobin@gmail.com>
2025-05-01 23:38:21 +0600

    new code added

--- code_practice/Operator.py
+++ code_practice/Operator.py
@@ -14,3 +14,15 @@ y = int(input("enter a another number:"))
 
 print("bitwise and :", x&y)
 print("bitwise OR", x|y)
+
+print("Comparisom number")
+p = int(input("number 'p' to compare : "))
+q = int(input("number 'q' to compare : "))
+if p == q:
+    print("equal")
+elif p > q:
+    print("p is greater than q")
+elif p < q:
+    print("p is less than q")
+else:
+    print("Not equal")
