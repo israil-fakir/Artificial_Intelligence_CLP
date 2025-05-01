c8b90620726d39ec72aae0190b26bb8229798826
israil-fakir <israilfakirrobin@gmail.com>
2025-05-01 23:30:40 +0600

    add new operator

--- code_practice/Operator.py
+++ code_practice/Operator.py
@@ -3,3 +3,14 @@ b = int(input("Ente a another number: "))
 
 print("sum :", a+b)
 print("sub :", a-b)
+print("mul :", a*b)
+print("div :", a/b)
+print("mod :", a%b)
+print("floor div :", a//b)
+print("exp :", a**b)
+
+x = int(input("enter a number: "))
+y = int(input("enter a another number:"))
+
+print("bitwise and :", x&y)
+print("bitwise OR", x|y)
