8b4acd6f6e1340aea8a146fbf9ba18e0f3c1a4a1
israil-fakir <israilfakirrobin@gmail.com>
2025-05-01 19:55:57 +0600

    add more code

--- code_practice/Variables.py
+++ code_practice/Variables.py
@@ -7,5 +7,26 @@ print(y)
 print(type(y))
 
 nam = "Ahemed"
+print(nam)
+print(type(nam))
+
+new_x = str(34)
+
+print(new_x)
+print(type(new_x))
+
+
+# st = int("string")
+# print(st)
+# print(type(st)) not working
+
+x, y, z = 1, 2, 3
+print(x, y, z) # varable and valuse must be same
+
+x = y =z = 99
+print(x)
+print(y)
+print(z)
+
 
 
