f61cf7cf82384b02fe52fb726036097da3acbf8f
israil-fakir <israilfakirrobin@gmail.com>
2025-05-02 23:36:16 +0600

    practicing the loop

--- code_practice/loop.py
+++ code_practice/loop.py
@@ -5,3 +5,25 @@ for i in range(x):
 
 for i in range(x, 0,-1):
     print("hello descending order:", i)
+
+
+for i in range(5, x):
+    print(i)
+
+
+for i in range(0, x, 2):
+    print("assending orderly two defference",i)
+
+for i in range(x, 0,-2):
+    print("descending orderly two defference", i)
+else:
+    print("loop end")
+
+
+fruits = ["orange", "bannana", "apple"]
+print("the fruits :")
+for i in fruits:
+    print(i, end=" ")
+
+
+for i 
