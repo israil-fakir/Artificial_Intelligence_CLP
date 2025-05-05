199ddb7b6a253e431ada49a96fbc76ecd24ba1f7
israil-fakir <israilfakirrobin@gmail.com>
2025-05-05 15:38:56 +0600

    add the files

--- code_practice/fun.py
+++ code_practice/fun.py
@@ -1,40 +1,35 @@
 
-def find_max(num):
-    list2 = []
-    for i in range(0, num):
-        num = int(input("enter the list value:"))
-        list2.append(num)
+def max_find(new_list, m):
+    for i in range(0, m):
+        for j in  range(i+1, m):
+            if new_list[i] < new_list[j]:
+                temp = new_list[i]
+                new_list[i] = new_list[j]
+                new_list[j] = temp
 
-    for i in range(0, num):
-        for j in range(i+1, num):
-            if list2[i] < list2[j]:
-                tm = list2[i]
-                list2[i] = list2[j]
-                list2[j] = tm
-
-    print(list2)
-
-    print("the max value:", list2[0])
+    print("The sorted new list: ",new_list)
+    print("Max value: ",new_list[0])
 
 
+def find_max_if(new_list, m):
+    max_val = new_list[0]
+    for i in range(0, m):
+        if i > max_val:
+            max_val = i
+    print("the max valuse using 'if' : ", max_val)
 
 
+n = int(input("enter the size: "))
+list2 = []
+for i in range(n):
+    ele = int(input("enter the elements :"))
+    list2.append(ele)
 
-
-
-def max_find_without_sorting:
-
-
-n = int(input("enter the list size: "))
-find_max(n)
-max_find_without_sorting(n)
-
-
-
-
-
-
-
-
+print("the elements is :",list2)
+max_find(list2, n)
+find_max_if(list2, n)
+list3 = sorted(list2,reverse=True)
+print("the sorted list :",list3)
+print("the max value useing sort_max", list3[0])
 
 
